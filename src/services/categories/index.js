0b2947cdcef74e5e062b5838ddf455fec167dc66
nostam <matsonho@gmail.com>
2021-01-21 16:56:30 +0100

    add sequelized CRUD for reviews and categories

--- src/services/categories/index.js
+++ src/services/categories/index.js
@@ -1,23 +1,24 @@
 const CategoriesRouter = require("express").Router();
 const { validationResult } = require("express-validator");
-const Model = require("../../utils/model");
-const Categories = new Model("categories");
 const { validateCategory } = require("../../utils/validate");
+const { Category } = require("../../utils/db");
 //TODO many2many for categories and articles
 
 CategoriesRouter.route("/")
   .get(async (req, res, next) => {
     try {
-      const { rows } = await Categories.find();
-      res.send(rows);
+      const categories = await Categories.findAll();
+      res.send(categories);
     } catch (e) {
       next(e);
     }
   })
   .post(validateCategory, async (req, res, next) => {
     try {
-      const response = await Categories.save(req.body);
-      res.status(201).send(response);
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) return next(err(errors.array(), 404));
+      const newCategory = await Categories.create(req.body);
+      res.status(201).send(newCategory);
     } catch (e) {
       next(e);
     }
@@ -26,8 +27,8 @@ CategoriesRouter.route("/")
 CategoriesRouter.route("/:id")
   .get(async (req, res, next) => {
     try {
-      const { rows } = await Categories.findById(req.params.id);
-      res.send(rows);
+      const category = await Category.findByPk(req.pararms.id);
+      res.send(category);
     } catch (e) {
       next(e);
     }
@@ -36,19 +37,21 @@ CategoriesRouter.route("/:id")
     try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) return next(err(errors.array(), 404));
-      const response = await Categories.findByIdAndUpdate(
-        req.params.id,
-        req.body
-      );
-      res.send(response);
+      const categoryArr = await Category.update(req.body, {
+        returning: true,
+        plain: true,
+        where: { id: req.params.id },
+      });
+      res.send(categoryArr[1]);
     } catch (e) {
       next(e);
     }
   })
   .delete(async (req, res, next) => {
     try {
-      const { rows } = await Authors.findByIdAndDelete(req.params.id);
-      res.send(rows);
+      const request = await Category.destroy({ where: { id: req.params.id } });
+      if (request === 0) next(err("ID not found", 404));
+      res.send("Deleted");
     } catch (e) {
       next(e);
     }
--- src/services/reviews/index.js
+++ src/services/reviews/index.js
@@ -1,14 +1,15 @@
 const ReviewsRouter = require("express").Router();
 const { validationResult } = require("express-validator");
-const Model = require("../../utils/model");
-const Reviews = new Model("reviews");
+const { Review, Author } = require("../../utils/db");
 const { err } = require("../../utils");
 const { validateReview } = require("../../utils/validate");
 ReviewsRouter.route("/")
   .get(async (req, res, next) => {
     try {
-      const { rows } = await Reviews.find();
-      res.send(rows);
+      const data = await Article.findAll({
+        include: Author,
+      });
+      res.send(data);
     } catch (e) {
       next(e);
     }
@@ -17,46 +18,39 @@ ReviewsRouter.route("/")
     try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) return next(err(errors.array(), 404));
-      // const request = {
-      //   ...req.body,
-      //   author_id: req.body.authorId,
-      //   article_id: req.body.articleId,
-      // };
-      // delete request.articleId;
-      // delete request.authorId;
-      const response = await Reviews.save(req.body);
-      res.status(201).send(response);
+      const newReview = await Review.create(req.body);
+      res.status(201).send();
     } catch (e) {
       next(e);
     }
   });
-
-ReviewsRouter.route("/:id")
-  .get(async (req, res, next) => {
-    try {
-      const { rows } = await Reviews.findById(req.params.id);
-      res.send(rows);
-    } catch (e) {
-      next(e);
-    }
-  })
-  .put(validateReview, async (req, res, next) => {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) return next(err(errors.array(), 404));
-      const response = await Reviews.findByIdAndUpdate(req.params.id, req.body);
-      res.send(response);
-    } catch (e) {
-      next(e);
-    }
-  })
-  .delete(async (req, res, next) => {
-    try {
-      const { rows } = await Reviews.findByIdAndDelete(req.params.id);
-      res.send(rows);
-    } catch (e) {
-      next(e);
-    }
-  });
+// duplicated with /articles route's
+// ReviewsRouter.route("/:id")
+//   .get(async (req, res, next) => {
+//     try {
+//       const { rows } = await Reviews.findById(req.params.id);
+//       res.send(rows);
+//     } catch (e) {
+//       next(e);
+//     }
+//   })
+//   .put(validateReview, async (req, res, next) => {
+//     try {
+//       const errors = validationResult(req);
+//       if (!errors.isEmpty()) return next(err(errors.array(), 404));
+//       const response = await Reviews.findByIdAndUpdate(req.params.id, req.body);
+//       res.send(response);
+//     } catch (e) {
+//       next(e);
+//     }
+//   })
+//   .delete(async (req, res, next) => {
+//     try {
+//       const { rows } = await Reviews.findByIdAndDelete(req.params.id);
+//       res.send(rows);
+//     } catch (e) {
+//       next(e);
+//     }
+//   });
 
 module.exports = ReviewsRouter;
